14fe81189f1bfc44684bd56a1b15b4f676486926
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-10-22 15:31:41 +0800

    New "roles" output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "email" {
   description = "The fully qualified email address of the created ServiceAccount."
   value       = google_service_account.service_account.email
 }
+
+output "roles" {
+  description = "All roles (except sensitive roles filtered by the module) that are attached to this ServiceAccount."
+  value       = tolist(local.filtered_roles)
+}
