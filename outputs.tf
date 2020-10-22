2c6667f7ad2f153070fa6a4ec19af93a61bcc28f
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-10-22 15:31:31 +0800

    Improve variable description

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "email" {
-  description = "The fully qualified email address of the created service account."
+  description = "The fully qualified email address of the created ServiceAccount."
   value       = google_service_account.service_account.email
 }
