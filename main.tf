99b89338b91d7926b52c8b3adf19f07553138e92
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2022-01-10 22:41:42 +0800

    Avoid redundant "sa-" portion in the SA names...
    ... will help to gain more character space in the SA names

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ terraform {
 }
 
 locals {
-  full_account_id   = format("%s-sa-%s", var.name, var.name_suffix)
-  full_display_name = format("%s ServiceAccount-%s", var.display_name, var.name_suffix)
+  full_account_id   = format("%s-%s", var.name, var.name_suffix)
+  full_display_name = format("%s %s", var.display_name, var.name_suffix)
   roles             = toset(var.roles)
   sensitive_roles   = ["roles/owner" /* we want to prevent terraform from granting sensitive roles to any resources */]
   filtered_roles    = setsubtract(local.roles, local.sensitive_roles)
