b436c16c114e6f0a2802d39feddd0ff79d8a585a
siddharthjain <siddharthjain@airasia.com>
2024-01-17 12:09:19 +0800

    format

--- variables.tf
+++ variables.tf
@@ -39,6 +39,6 @@ variable "roles" {
 
 variable "full_account_id" {
   description = "The account id that is used to generate the service account email address and a stable unique id. Must be 6-30 characters long, and match the regular expression [a-z]([-a-z0-9]*[a-z0-9])."
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
